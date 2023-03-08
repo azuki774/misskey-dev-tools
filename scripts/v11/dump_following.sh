3dd4ff05b4299f718bb5369d2a4307e82eb40fe3
azuki774 <azuki774s@gmail.com>
2023-03-08 18:27:01 +0900

    apply v11

---
+++ scripts/v11/dump_following.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+USERNAME=${USERNAME}  # misskey username
+LOG_DIR=${LOG_DIR}    # log directory
+ROOT_DIR=${ROOT_DIR}  # save json directory
+ADDRESS=${ADDRESS}    # ex. misskey.dev
+
+LOG_FILE="${LOG_DIR}/dump_misskey.log"
+YYYYMM=`date '+%Y%m'`
+YYYYMMDD=`date '+%Y%m%d'`
+
+FILE=${ROOT_DIR}/${YYYYMM}/${YYYYMMDD}_following.json
+
+mkdir -p ${ROOT_DIR}/${YYYYMM}
+
+URL="https://${ADDRESS}/api/users/following"
+
+echo "`date`: Start sciprts" > ${LOG_FILE}
+curl -H "Content-Type: application/json" -d "{\"username\": \"${USERNAME}\" }" ${URL} -o ${FILE}
+echo "`date`: Save following jsonfile: ${FILE}" >> ${LOG_FILE}
+echo "`date`: End sciprts" >> ${LOG_FILE}
