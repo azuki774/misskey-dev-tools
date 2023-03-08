e61a01663d6959da6672ba8fffbf9f3eb0235689
azuki774 <azuki774s@gmail.com>
2023-03-08 18:43:29 +0900

    apply v13

---
+++ scripts/v13/dump_followers.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+USERID=${USERID}      # misskey userid
+LOG_DIR=${LOG_DIR}    # log directory
+ROOT_DIR=${ROOT_DIR}  # save json directory
+ADDRESS=${ADDRESS}    # ex. misskey.dev
+
+LOG_FILE="${LOG_DIR}/dump_misskey.log"
+YYYYMM=`date '+%Y%m'`
+YYYYMMDD=`date '+%Y%m%d'`
+
+FILE=${ROOT_DIR}/${YYYYMM}/${YYYYMMDD}_followers.json
+
+mkdir -p ${ROOT_DIR}/${YYYYMM}
+
+URL="https://${ADDRESS}/api/users/followers"
+
+echo "`date`: Start sciprts" > ${LOG_FILE}
+curl -H "Content-Type: application/json" -d "{\"username\": \"${USERNAME}\" }" ${URL} -o ${FILE}
+echo "`date`: Save followers jsonfile: ${FILE}" >> ${LOG_FILE}
+echo "`date`: End sciprts" >> ${LOG_FILE}
